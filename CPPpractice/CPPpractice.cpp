202ae404c41e1964b60892f4cfbcf1fbd4627192
kwh0163 <kwh0163@gmail.com>
2022-10-13 20:21:52 +0900

    Add project files.

---
+++ CPPpractice/CPPpractice.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+int main()
+{
+	char alphabet = 'B';
+	int value = 10;
+	float variable = 10.5;
+
+	std::cout << alphabet << std::endl;
+	std::cout << value << std::endl;
+	std::cout << variable << std::endl;
+
+	std::cout << "두 번째 안녕하세요.";
+
+}
